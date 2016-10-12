1c2759029d91bb93cd87a07a8ebe06dc4c9242b6
Nieobliczalny <krychu1993@gmail.com>
2016-10-12 22:18:48 +0200

    Dodanie widoku podstawowych rzeczy (tablica, lista, zadanie, zalogowany użytkownik)

--- js/view-models/index.js
+++ js/view-models/index.js
@@ -1,6 +1,11 @@
 // Here's my data model
 var IndexViewModel = function() {
     this.sampleText2 = ko.observable();
+	this.loggedUserName = ko.observable('User001');
+	this.lists = ko.observableArray([
+	{name: 'Lista 1', tasks: [{name: 'Task 1'},{name: 'Task 2'},{name: 'Task 3'},{name: 'Task 4'}]}, {name: 'Lista 2', tasks: [{name: 'Task 1'},{name: 'Task 2'}]}
+	]);
+	this.board = ko.observable({name: 'Board 1'});
  
     this.sampleText = ko.pureComputed(function() {
         return '> ' + this.sampleText2() + ' <';
