b53acb38041e706114c973273323f749b09e0848
Nieobliczalny <krychu1993@gmail.com>
2016-10-24 00:09:37 +0200

    Dodanie uzycia serwisu zamiast sztywnych zadan http

--- js/view-models/index.js
+++ js/view-models/index.js
@@ -5,67 +5,49 @@ var IndexViewModel = function() {
 	this.lists = ko.observableArray([]);
 	this.board = ko.observable({});
 	
-	var apiPrefix = 'http://localhost/paw-server/web/app_dev.php/api/';
+	var apiPrefix = 'http://localhost:8080/paw-server/web/app_dev.php/api/';
+	var TrelloApi = new TrelloRestService(apiPrefix);
 	
 	var self = this;
 	
-	$.ajax({
-		url: apiPrefix + 'logins',
-		dataType: 'json',
-		method: 'POST',
-		data: {login: 'Adam', password: 'admin'}
-	})
+	TrelloApi.logins.post({login: 'Adam', password: 'admin'}, function(result){console.log(result)}, function(error){console.info(error);});
 	
-	//TODO: Refaktoryzacja celem przygotowania obiektu API bez każdorazowego wywołania $.ajax
-	$.ajax({
-		url: apiPrefix + 'boards/1',
-		dataType: 'json',
-		method: 'GET'
-	}).done(function(data){
-		self.board(data);
-		$.ajax({
-			url: apiPrefix + 'boards/1/lists',
-			dataType: 'json',
-			method: 'GET'
-		}).done(function(data){
+	TrelloApi.boards.getById(1, function(result){
+		self.board(result);
+		TrelloApi.boards.at(1).lists.get(function(data){
 			var lists = [];
 			for (var i = 0; i < data.length; i++)
 			{
 				lists.push({name: data[i].name, tasks: ko.observableArray([])});
 				(function(){
 					var iCopy = i;
-					$.ajax({
-						url: apiPrefix + 'lists/' + data[iCopy].id + '/tasks',
-						dataType: 'json',
-						method: 'GET'
-					}).done(function(data){
-						for (var i = 0; i < data.length; i++)
+					TrelloApi.lists.at(data[i].id).tasks.get(function(result){
+						for (var i = 0; i < result.length; i++)
 						{
-							self.lists()[iCopy].tasks.push(data[i]);
+							self.lists()[iCopy].tasks.push(result[i]);
 						}
-					}).fail(function(error){
+					}, function(error){
 						console.error(error);
 					});
 				})();
 			}
 			self.lists(lists);
-		}).fail(function(error){
+		}, function(error){
 			console.error(error);
 		});
-	}).fail(function(error){
+	}, function(error){
 		console.error(error);
 	});
 	
-	$.ajax({
-		url: apiPrefix + 'loggeduser',
-		dataType: 'json',
-		method: 'GET'
-	}).done(function(data){
+	TrelloApi.loggeduser.get(function(data){
 		self.loggedUserName(data.username);
-	}).fail(function(error){
+	}, function(error){
 		console.error(error);
 	});
- 
+	
+	//TrelloApi.boards.put(1, {}, function(data){ console.info('.>', data) }, function(error){console.error('.>', error)});
+	//TrelloApi.boards.delete(1, function(data){ console.info('..>', data) }, function(error){console.error('..>', error)});
+
     this.sampleText = ko.pureComputed(function() {
         return '> ' + this.sampleText2() + ' <';
     }, this);
